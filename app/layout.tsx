2a8de31c6bdd85e142e5c48b32ae0760d7b757f4
sabrrt777 <SABRRT777@Gmail.COM>
2025-08-27 14:46:53 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,8 @@ const cairo = Cairo({
 })
 
 export const metadata: Metadata = {
-  title: "دليل أفراد الشرطة",
-  description: "موقع دليل أفراد الشرطة والقيادات العسكرية",
-  generator: "v0.app",
+  title: "Respect Police",
+  description: "RT - Police",
 }
 
 export default function RootLayout({
